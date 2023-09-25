fd4b44238ecea25c9bfd9e6ca58165b6bb53a26f
ChernenkiyNikitych4732 <chernenkiy.nikitychsssr@gmail.com>
2023-09-25 09:40:52 +0200

    2 Курс Курсовая работа №2 Java Core-изначальный комментарий

---
+++ src/main/java/sky/pro/course2/coursework/Course2Coursework2JavaCoreMastermindApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.course2.coursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2Coursework2JavaCoreMastermindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course2Coursework2JavaCoreMastermindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/course2/coursework/Course2Coursework2JavaCoreMastermindApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.course2.coursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2Coursework2JavaCoreMastermindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
